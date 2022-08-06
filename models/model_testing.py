3edf66acb4bd8146277b43e32f1e3938fe5bad1d
Patrick Willson <patrick@LAPTOP-0MTDJUQ5.localdomain>
2022-08-06 18:51:04 +0100

    added basic cross validation to model testing

--- models/model_testing.py
+++ models/model_testing.py
@@ -2,41 +2,55 @@ import yfinance as yf
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-#test the models
-def test_models(models):
+#test the models with K-fold cross validation
+def test_models(all_data, all_results, k=10):
     global history
 
-    for b in range(0, 2):
-        data = []
-        results = [] 
-        for i in range(len(history)-3000, len(history)):
-            data.append([history[i][b]])
-            if i > 3:
-                results.append(history[i][b])
-            for j in range(2, min(len(data)+1, 5)):
-                data[-j].append(history[i][b]) 
+    accuracies = []
+    models = []
 
-        if len(data) == len(results):
-            data = data[:-3]
-            results = results[:-3]
-        else:
-            data = data[:len(results)]
+    for b in range(len(all_results)):
 
-        predicated_results = []
-        for d in range(len(data)):
-            predicated_results.append(models[b].predict([data[d]]))
-        percentage_inaccuracy = []
-        for p in range(len(predicated_results)):
-            percentage_inaccuracy.append(abs(1-(predicated_results[p]/results[p])))
-        print(np.mean(percentage_inaccuracy))
+        items_per_fold = int(len(all_results[b]) / k)
+        extra = len(all_results[b]) % k
+
+        for f in range(k):
+
+            test = all_data[b][f*items_per_fold:(f+1)*items_per_fold]
+            test_results = all_results[b][f*items_per_fold:(f+1)*items_per_fold]
+
+            data_array = all_data[0:f*items_per_fold]
+            data_array.extend(all_data[(f+1)*items_per_fold:])
+            results_array = all_results[0:f*items_per_fold]
+            results_array.extend(all_data[(f+1)*items_per_fold:])
+
+            for i in range(len(results_array)):
+
+                models.append(LinearRegression())
+                models[-1].fit(data, results)
+
+                predicated_results = []
+                for d in range(len(test)):
+                    predicated_results.append(models[b].predict([test[d]]))
+                    # print(predicated_results[-1])
+                percentage_inaccuracy = []
+                for p in range(len(predicated_results)):
+                    percentage_inaccuracy.append(abs(1-(predicated_results[p]/test_results[p])))
+                # print(percentage_inaccuracy)
+                accuracies.append(np.mean(percentage_inaccuracy))
+    
+    print(np.mean(accuracies))
 
 
 if __name__ == "__main__":
 
+    num_train = 100
+
     stock = yf.Ticker("BAC")
     history = stock.history(period='max')[["High", "Low"]].to_numpy()
 
-    models = []
+    all_data = []
+    all_results = []
 
     #build models
     for b in range(0,2):
@@ -44,21 +58,21 @@ if __name__ == "__main__":
         data = []
         results = []
 
-        for i in range(len(history)-3000):
+        for i in range(len(history)):
             data.append([history[i][b]])
-            if i > 3:
+            if i > num_train:
                 results.append(history[i][b])
-            for j in range(2, min(len(data)+1, 5)):
+            for j in range(2, min(len(data)+1, num_train)):
                 data[-j].append(history[i][b])
 
         if len(data) == len(results):
-            data = data[:-3]
-            results = results[:-3]
+            data = data[:-num_train]
+            results = results[:-num_train]
         else:
             data = data[:-1][:len(results)]
 
-        models.append(LinearRegression())
-        models[-1].fit(data, results)
+        all_data.append(data)
+        all_results.append(results)
         
-    test_models(models)
+    test_models(all_data, all_results)
 
